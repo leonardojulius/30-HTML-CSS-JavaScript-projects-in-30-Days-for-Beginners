3e9f8d3633f69907f97db6c03cde7b585ea01f0b
leonardojulius <leonardojulius.jcl@gmail.com>
2022-10-02 11:47:19 +0800

    main js unfinished

---
+++ 2. I am NOT a Robot (Beginner project)/main.js
@@ -0,0 +1,15 @@
+
+
+const checkbox = document.getElementById("checkbox");
+const submitBtn = document.querySelector("button[type=submit]");
+
+
+// disable checkbox
+checkbox.disabled = true;
+submitBtn.disabled = true;
+console.log(submitBtn);
+
+const elements = document.querySelectorAll(".element");
+console.log(elements);
+
+

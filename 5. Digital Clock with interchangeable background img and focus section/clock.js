b5cd8f0fd79a06509581dad84d65066741e0c417
leonardojulius <leonardojulius.jcl@gmail.com>
2022-10-08 21:12:21 +0800

    clock js

--- 5. Digital Clock with interchangeable background img and focus section/clock.js
+++ 5. Digital Clock with interchangeable background img and focus section/clock.js
@@ -34,8 +34,48 @@ function showTime(){
  s = s <10 ? (s = "0" + s) : s;
 
 
- const time = h + ":" + m + ":" + s + "" + "|" + "" + session;
+
+
+const time=`${h}:${m}<small>.${s}|${session}</small>`;
 document.getElementById("DisplayClock").innerHTML = time;
 setTimeout(showTime,1000);
 
+
+//Change Body Background
+let bg;
+
+const userName = "Nobert";
+const user = document.getElementById("User");
+
+if(h<8 && session==="AM"){
+  bg = `url(https://source.unsplash.com/G_a9JhmiZFk)`;
+    user.innerHTML = `Good morning ${userName}`;
+} else if(h<11 && session ==="AM") {
+
+  bg = `url(https://source.unsplash.com/XGzW_tr_8YI)`;
+  user.innerHTML = `Good day ${userName}`;
 }
+
+else if(h<8 && session ==="PM") {
+
+  bg = `url(https://source.unsplash.com/Cjz1UAz8VBs)`;
+   user.innerHTML = `Good afternoon ${userName}`;
+ }
+ else {
+
+  bg = `url(https://source.unsplash.com/ln5drpv_ImI)`;
+   user.innerHTML = `Good Evening ${userName}`;
+ }
+
+ const body = document.querySelector("body");
+
+ body.style.background = `${bg} center/cover`
+
+}
+
+document.querySelector(".focus-container input").addEventListener("keypress",function(event){
+   if(event.key==="Enter") {
+    const focus = document.querySelector(".focus-container input");
+    document.querySelector(".focus-container").innerHTML=`<h6> TODAY:</h6><h1>${focus.value}</h1>`
+   }
+});

4a2752074bbf131a97523a5266c296b962d6aa2f
leonardojulius <leonardojulius.jcl@gmail.com>
2022-10-10 00:48:18 +0800

    Animated Navigation Finshed

--- 6. Animated Navigation/nav.js
+++ 6. Animated Navigation/nav.js
@@ -7,10 +7,29 @@ menuToggle.addEventListener('click', function(){
         activeElements[active].classList.toggle("active");
     }
 
-  //  console.log(menuToggle.childNodes[1]);
+    console.log(menuToggle.childNodes[1]);
 
     const navBtnIcon = menuToggle.childNodes[1];
     navBtnIcon.classList.toggle("fa-times");
 
+    const pageBg = document.querySelector(".main-container");
 
+    if(navBtnIcon.classList.contains("fa-times")) {
+        pageBg.style.filter="blur(3px)";
+    
+      
+    }else{
+
+        setTimeout(function(){
+            pageBg.style.filter="blur(0px)";
+        },500);
+     }
+
+     const listItems = document.querySelectorAll(".main-nav-list a");
+  //   console.log(listItems);
+
+     listItems.forEach(function (item,index){
+       // console.log(item.style.transition = `all .5s linear ${index * 0.3}s`);
+        item.style.transition = `all .5s linear ${index * 0.3}s`;
+    });
 });

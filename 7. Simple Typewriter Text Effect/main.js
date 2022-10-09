8ef68d0d4f3ff87be4f5ed89ed14c6f4c3972eff
leonardojulius <leonardojulius.jcl@gmail.com>
2022-10-10 01:38:30 +0800

    Typewriter Effect main.js Unfinished

---
+++ 7. Simple Typewriter Text Effect/main.js
@@ -0,0 +1,12 @@
+let text = document.getElementById("Typewriter");
+
+text = document.querySelector("#Typewriter h2");
+const textArray = text.textContent.split("");
+
+console.log(textArray)
+
+let timeLopper;
+
+text.innerHTML = "";
+
+console.log(object)

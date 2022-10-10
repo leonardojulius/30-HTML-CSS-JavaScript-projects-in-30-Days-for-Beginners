b6c9707b170d67c7307ccd366b49a88b78d6923e
leonardojulius <leonardojulius.jcl@gmail.com>
2022-10-11 02:02:18 +0800

    Typewriter Effect

--- 7. Simple Typewriter Text Effect/main.js
+++ 7. Simple Typewriter Text Effect/main.js
@@ -1,12 +1,27 @@
 let text = document.getElementById("Typewriter");
+//text = document.querySelector("#Typewriter h2");
 
-text = document.querySelector("#Typewriter h2");
-const textArray = text.textContent.split("");
+const textArray =  text.textContent.split("");
 
-console.log(textArray)
+console.log(textArray); 
 
-let timeLopper;
+let timeLooper;
 
-text.innerHTML = "";
+text.innerHTML ="";
 
-console.log(object)
+console.log(text);
+
+function typewriterEffect() {
+
+    if(textArray.length>0) {
+        text.innerHTML += textArray.shift();
+    }else{
+        clearTimeout(timeLooper);
+    }
+
+    timeLooper=setTimeout(
+        "typewriterEffect()", Math.floor(Math.random()*(200,250))
+        
+        );
+}
+typewriterEffect() ;
